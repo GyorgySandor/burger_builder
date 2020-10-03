9c9d815c01964159ff968e9ee8542743f54398a7
GyorgySandor <sgyorgy91@gmail.com>
2020-10-03 13:43:47 +0200

    130 Calculating the ingredient sum

--- burgerbuilder/src/components/Burger/Burger.js
+++ burgerbuilder/src/components/Burger/Burger.js
@@ -7,14 +7,23 @@ const burger = (props) => {
     Here, with the Object.keys returns the keys as array of strings.
     1.Than we map though that array
     2. on each key, we return an array as big as the number for the key is in the original ingredient object
-    3. the array contains as man BurgerIngredients 
-    
+    3. the array contains as man BurgerIngredients  
     */
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
