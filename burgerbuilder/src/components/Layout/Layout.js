9a77447bd9f1e02ac2f10a0415679b84d33e2b1e
GyorgySandor <sgyorgy91@gmail.com>
2020-10-03 10:03:41 +0200

    125 BurgerBuilder container component

--- burgerbuilder/src/components/Layout/Layout.js
+++ burgerbuilder/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Ausiliary from "../../hoc/Ausiliary";
+import classes from "./Layout.css";
 
 const Layout = (props) => (
   <Ausiliary>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Ausiliary>
 );
 
