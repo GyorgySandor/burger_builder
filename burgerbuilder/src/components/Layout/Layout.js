65d98587f1e9d93ba86c2eb63f9e7b910aee49dc
GyorgySandor <sgyorgy91@gmail.com>
2020-10-05 10:40:26 +0200

    143 Addign the Toolbar

--- burgerbuilder/src/components/Layout/Layout.js
+++ burgerbuilder/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Ausiliary from "../../hoc/Ausiliary";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const Layout = (props) => (
   <Ausiliary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar>Toolbar, SideDrawer, Backdrop</Toolbar>
     <main className={classes.Content}>{props.children}</main>
   </Ausiliary>
 );
