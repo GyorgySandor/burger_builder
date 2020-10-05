ba7d982814092d2ab414f722f5db206077c494c5
GyorgySandor <sgyorgy91@gmail.com>
2020-10-05 10:02:02 +0200

    142 adding the total price to order summary

--- burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,9 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingreientSummary}</ul>
+      <p>
+        <strong>Total Price:{props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to chekcout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>
         CANCEL
