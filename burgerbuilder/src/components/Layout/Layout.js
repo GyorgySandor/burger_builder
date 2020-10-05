a1109225ed3c06fb2ea4e56c7833c109142a48bc
GyorgySandor <sgyorgy91@gmail.com>
2020-10-05 14:13:10 +0200

    146 creating a responsive sidedrawer

--- burgerbuilder/src/components/Layout/Layout.js
+++ burgerbuilder/src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from "react";
 import Ausiliary from "../../hoc/Ausiliary";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) => (
   <Ausiliary>
-    <Toolbar>Toolbar, SideDrawer, Backdrop</Toolbar>
+    <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Ausiliary>
 );
